20c350ba36c05d7811aa718c4424e1c87bf8a0b4
abdurrahim51 <92558689+abdurrahim51@users.noreply.github.com>
2021-10-31 10:27:43 +0700

    Add files via upload

---
+++ Penugasan.java
@@ -0,0 +1,66 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tugas;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author MATRIX COMPUTER
+ */
+public class Penugasan {
+     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        
+        System.out.println("Masukan Nilai A : " );
+        int nilaiA = input.nextInt();
+        System.out.println("Masukun Nilai B : " );
+        int nilaiB = input.nextInt();
+        int hasil_pengisian_nilai = nilaiA = nilaiB;
+        System.out.println("Hasil Pengisian Nilai = ");
+        
+        
+        System.out.println("Masukan Nilai C : " );
+        int nilaiC = input.nextInt();
+        System.out.println("Masukun Nilai D : " );
+        int nilaiD = input.nextInt();
+        int hasil_pengisian_dan_pertambahan = nilaiA = nilaiB;
+        System.out.println("Hasil Pengisian dan pertambahan = ");
+        
+        
+        System.out.println("Masukan Nilai E : " );
+        int nilaiE = input.nextInt();
+        System.out.println("Masukun Nilai F : " );
+        int nilaiF = input.nextInt();
+        int hasil_pengisian_dan_pengurangan = nilaiA = nilaiB;
+        System.out.println("Hasil Pengisian dan pengurangan = ");
+        
+        
+        System.out.println("Masukan Nilai G : " );
+        int nilaiG = input.nextInt();
+        System.out.println("Masukun Nilai H : " );
+        int nilaiH = input.nextInt();
+        int hasil_pengisian_dan_perkalian = nilaiC = nilaiD;
+        System.out.println("Hasil Pengisian dan perkalian = " );
+        
+        
+        System.out.println("Masukan Nilai I : " );
+        int nilaiI = input.nextInt();
+        System.out.println("Masukun Nilai J : " );
+        int nilaiJ = input.nextInt();
+        int hasil_pengisian_dan_pembagian = nilaiE = nilaiF;
+        System.out.println("Hasil Pengisian dan pembagian = " );
+        
+        
+        System.out.println("Masukan Nilai K : " );
+        int nilaiK = input.nextInt();
+        System.out.println("Masukun Nilai L : " );
+        int nilaiL = input.nextInt();
+        int hasil_pengisian_dan_hasil_bagi = nilaiG = nilaiH;
+        System.out.println("Hasil Pengisian dan hasil bagi = " );
+        
+     }    
+}
